09a1c5a7a330c8d0ba3d6fa7cabd5e70bff7bfc4
Guxtavoc <gustavocurado1234@gmail.com>
2026-05-09 23:11:05 -0400

    aplicação flask integrada ao modelo CNN

--- analise.py
+++ analise.py
@@ -6,22 +6,35 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
 model = load_model("modelo/modelo.keras")
-
 class_names = ['Capacitor', 'IC', 'Resistor', 'Transistor']
-img_path = "teste/resistor.jpg"
 
-img = image.load_img(
-    img_path,
-    target_size=(128, 128)
-)
+def analise(caminho):
 
-img_array = image.img_to_array(img)
-img_array = np.expand_dims(img_array, axis=0)
-prediction = model.predict(img_array)
-predicted_class = np.argmax(prediction)
-confidence = np.max(prediction) * 100
+    #caminho = "static/uploads/imagem.jpg"
 
-print(f"Classe prevista: {class_names[predicted_class]}")
-print(f"Confiança: {confidence:.2f}%")
-for i, classe in enumerate(class_names):
-    print(f"{classe}: {prediction[0][i] * 100:.2f}%")
+    img = image.load_img(
+        caminho,
+        target_size=(128, 128)
+    )
+
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    prediction = model.predict(img_array)
+    predicted_class = np.argmax(prediction)
+    confidence = np.max(prediction) * 100
+    resultado = {
+        'class_name': class_names[predicted_class],
+        'confidence': round(confidence, 2),
+        'predicted_class_index': int(predicted_class),
+        'probabilities': []
+    }
+
+    for i, classe in enumerate(class_names):
+        probabilidade = float(prediction[0][i] * 100)
+        resultado['probabilities'].append({
+            'class': classe,
+            'probability': round(probabilidade, 2),
+            'index': i
+        })
+    
+    return resultado
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from analise import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/analisar", methods=["POST"])
+def analisar():
+
+    imagem = request.files["imagem"]
+
+    caminho = "static/uploads/" + "imagem.jpg"
+
+    imagem.save(caminho)
+
+    resultado = analise(caminho)
+
+    return render_template ("resultado.html",resultado=resultado)
+
+if __name__ == "__main__":
+    app.run(debug=True)
